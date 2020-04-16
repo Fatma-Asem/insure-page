93482e05bd99915c0187bcfe1bb79b8aa592d160
Fatma Asem <fatmaasem1234@gmail.com>
2020-04-16 13:55:37 +0200

    Initial commit

---
+++ js/landing-page.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    // add nice scroll to body
+    $('body').niceScroll({
+        cursorcolor:'#000',
+        cursorwidth:'10px',
+        cursorborder:'none'
+    });
+
+    
+});
